ef09ca55825a90c51aa8bf736c9621cde0e3d935
fredkingham <fredkingham@gmail.com>
2017-03-29 20:25:09 +0100

    revert the database to mysql

--- config/database.php
+++ config/database.php
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    // 'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
